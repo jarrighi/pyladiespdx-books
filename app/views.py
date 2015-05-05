c36122bc9c2e354283453ac53905346a920e0386
Jules Arrighi <juliana.arrighi@gmail.com>
2015-05-04 21:41:22 -0700

    Add a book detail page and content to templates

--- app/views.py
+++ app/views.py
@@ -3,4 +3,23 @@ from flask import render_template
 
 @app.route('/')
 def get_books():
-    return render_template('books.html')
+    books = [{"title": "Book", "author": "Person"}, 
+             {"title": "Book", "author": "Person"},
+             {"title": "Book", "author": "Person"},
+             {"title": "Book", "author": "Person"}]
+    return render_template('books.html', books=books)
+
+@app.route('/books/<book>')
+def book_page(book):
+    
+    # The following data should eventually get passed into the page
+    # 
+    # Title and Author of the book
+    # Who has the book
+    # Links to information about the book
+    # tags that describe the book
+    # butts
+    # button to request the book
+    book_details = {'title': 'python book', 'author': 'pythonista', 
+                    'possessor': 'pyltergeist'}
+    return render_template('book_detail.html', details=book_details)
