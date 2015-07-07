c10393517d8da52a89258a6234a899cca600c23e
Jules Arrighi <juliana.arrighi@gmail.com>
2015-07-06 22:27:27 -0700

    modify database settings method to use with Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/librarpy"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from app import views, models
